c3dc26c8aa4128ae0a55b6a461dcc695c9ce728f
w0lf <smackywolf@users.noreply.github.com>
2015-03-31 09:03:57 +1100

    Update content_script.js

--- Source/content_script.js
+++ Source/content_script.js
@@ -36,6 +36,55 @@ function handleText(textNode)
 	v = v.replace(/\bthe Cloud\b/g, "my Butt");
 	v = v.replace(/\bthe cloud\b/g, "my butt");
 	
+	// add SJW bullshit
+	v = v.replace(/\bsjws\b/g, "skeletons");
+	v = v.replace(/\bSJWs\b/g, "skeletons");
+	v = v.replace(/\bSjws\b/g, "skeletons");
+	v = v.replace(/\bSJws\b/g, "skeletons");
+	v = v.replace(/\bSJWs\b/g, "skeletons");
+	// also bad grammar
+	v = v.replace(/\bsjw's\b/g, "skeletons");
+	v = v.replace(/\bSJW's\b/g, "skeletons");
+	v = v.replace(/\bSjw's\b/g, "skeletons");
+	v = v.replace(/\bSJw's\b/g, "skeletons");
+	v = v.replace(/\bSJW's\b/g, "skeletons");
+	// lol
+	v = v.replace(/\bsjw\b/g, "skeleton");
+	v = v.replace(/\bSJW\b/g, "skeleton");
+	v = v.replace(/\bSjw\b/g, "skeleton");
+	v = v.replace(/\bSJw\b/g, "skeleton");
+	v = v.replace(/\bSJW\b/g, "skeleton");
+	
+	
+	// add Big Data
+	v = v.replace(/\bBig Data\b/g, "Big Dicks");
+	v = v.replace(/\bbig data\b/g, "big dicks");
+	v = v.replace(/\bbig Data\b/g, "big Dicks");
+	v = v.replace(/\bBig data\b/g, "Big dicks");
+	
+	// add buttcoin crap
+	v = v.replace(/\bBitcoin\b/g, "Buttcoin");
+	v = v.replace(/\bBITCOINS\b/g, "BUTTCOINS");
+	v = v.replace(/\bbitcoin\b/g, "buttcoin");
+	v = v.replace(/\bBitcoins\b/g, "Buttcoins");
+	v = v.replace(/\bBTC\b/g, "Buttcoin");
+	v = v.replace(/\bBitCoin\b/g, "ButtCoin");
+	
+	// "the internet of things" is pretty fuckin dumb, right?
+	v = v.replace(/\bThe internet of things\b/g, "The internet of dongs");
+	v = v.replace(/\bThe Internet Of Things\b/g, "The Internet Of Dongs");
+	v = v.replace(/\bThe internet of Things\b/g, "The internet of Dongs");
+	v = v.replace(/\bThe Internet of things\b/g, "The Internet of dongs");
+	v = v.replace(/\bThe Internet of Things\b/g, "The Internet of Dongs");
+	
+	// HE MAN
+	v = v.replace(/\bThe patriachy\b/g, "Castle Grayskull");
+	v = v.replace(/\bThe Patriachy\b/g, "Castle Grayskull");
+	v = v.replace(/\bthe Patriachy\b/g, "Castle Grayskull");
+	v = v.replace(/\bTHE PATRIACHY\b/g, "Castle Grayskull");
+	v = v.replace(/\bpatriachy\b/g, "Castle Grayskull");
+	v = v.replace(/\bPATRIACHYs\b/g, "Castle Grayskull");
+	
 	textNode.nodeValue = v;
 }
 
